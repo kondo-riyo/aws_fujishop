a535a970e15e5ab9a95a54862900a74415ad058e
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2021-10-15 09:34:16 +0900

    テストを実施しました

---
+++ test/pages/orderLog.spec.js
@@ -0,0 +1,108 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import OrderLog from '../../pages/OrderLog.vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+import VueMeta from 'vue-meta';
+
+import { createStore } from '../../.nuxt/store';
+import { initialiseStores } from '../../utils/store-accsessor.ts';
+import { jsxEmptyExpression } from '@babel/types';
+
+const localVue = createLocalVue();
+const router = new VueRouter();
+localVue.use(VueRouter);
+localVue.use(Vuex);
+localVue.use(VueMeta, { keyName: 'head' });
+describe('Testing OrderLog component', () => {
+  let wrapper;
+  let store;
+  let CartStore;
+
+  beforeAll(() => {
+    initialiseStores(createStore());
+
+    let fn = jest.fn();
+    CartStore = {
+      namespaced: true,
+      getters: {
+        getOrderLog: fn,
+      },
+      actions: {
+        fetchitemInfoAct: fn,
+        cancelOrderAct: fn,
+      },
+    };
+
+    store = new Vuex.Store({
+      modules: {
+        cart: CartStore,
+      },
+    });
+    wrapper = shallowMount(OrderLog, {
+      localVue,
+      router,
+      store,
+      computed: {
+        totalItemPrice() {
+          let totalPrice = 0;
+          this.getOrderLogs.forEach((item) => {
+            item.itemInfo.forEach((price) => {
+              totalPrice = totalPrice + price.totalPrice;
+            });
+          });
+          return totalPrice;
+        },
+        getOrderLogs() {
+          return [
+            {
+              orderInfo: { allPrice: 0 },
+              itemInfo: [{ totalPrice: 400 }, { totalPrice: 400 }],
+              status: 1,
+            },
+          ];
+        },
+      },
+    });
+  });
+  it('OrderLogが存在する', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+  it('headが存在する', () => {
+    expect(wrapper.vm.$metaInfo.title).toBe('注文履歴');
+  });
+  it('totalItemPriceが期待通りか？', async () => {
+    await expect(wrapper.vm.totalItemPrice).toEqual(800);
+  });
+  it('getOrderLogsが期待通りか？', async () => {
+    await expect(wrapper.vm.getOrderLogs).toEqual([
+      {
+        itemInfo: [{ totalPrice: 400 }, { totalPrice: 400 }],
+        orderInfo: { allPrice: 0 },
+        status: 1,
+      },
+    ]);
+  });
+  it('closeModalがemitされる', () => {
+    let closeWrapper = wrapper.find('order-modal-stub');
+    closeWrapper.vm.$emit('close');
+    expect(closeWrapper.trigger('closeModal')).toBeTruthy();
+  });
+
+
+
+  it('cickでイベント(cancelOrder)が発火する',async () => {
+    let cancelWrapper = wrapper.find('[data-testid="cancelOrder"]');
+    cancelWrapper.trigger('click')
+    //cancelWrapper.vm.$emit('click');
+    expect(cancelWrapper.trigger('cancelOrder')).toBeTruthy();
+  // await expect(CartStore.actions.cancelOrderAct).toBeCalled()
+
+  })
+  
+  ;
+  it('cickでイベント(openModal)が発火する', () => {
+    let cancelWrapper = wrapper.find('square-bottun-stub');
+    cancelWrapper.vm.$emit('click');
+    expect(cancelWrapper.trigger('openModal')).toBeTruthy();
+  });
+});
